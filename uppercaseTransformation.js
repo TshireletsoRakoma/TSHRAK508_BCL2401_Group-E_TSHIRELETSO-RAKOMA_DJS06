39e631580eb9eddc6b80b26b93047b14493e0cad
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-05-24 14:11:50 +0200

    "Added implementation to uppercaseTransformation function and exported it as a module"

--- uppercaseTransformation.js
+++ uppercaseTransformation.js
@@ -4,3 +4,9 @@
 function uppercaseTransformation(provinces) {
     // Creating a new array of province names in uppercase
     console.log("\nUppercase Transformation:");
+
+    const uppercaseProvinces = provinces.map(province => province.toUpperCase());
+    console.log(uppercaseProvinces);
+}
+
+module.exports = uppercaseTransformation;
