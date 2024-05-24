19937c1de51430750df3afee16d08ba8dbbc553d
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-05-24 14:09:30 +0200

    created uppercasetransformation.js file and added function for uppercase

---
+++ uppercaseTransformation.js
@@ -0,0 +1,6 @@
+// uppercaseTransformation.js
+
+// Uppercase Transformation
+function uppercaseTransformation(provinces) {
+    // Creating a new array of province names in uppercase
+    console.log("\nUppercase Transformation:");
