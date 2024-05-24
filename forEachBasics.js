a1ad15705a5ea687a55d4080e12cf0b073f121ad
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-05-24 14:06:11 +0200

    "Added forEachBasics function to log names and provinces using forEach"

---
+++ forEachBasics.js
@@ -0,0 +1,10 @@
+// forEachBasics.js
+
+// ForEach Basics
+function forEachBasics(names, provinces) {
+  // Logging each name and province
+  console.log("ForEach Basics:");
+  console.log("Names:");
+  names.forEach(name => console.log(name));
+  console.log("\nProvinces:");
+  provinces.forEach(province => console.log(province));
