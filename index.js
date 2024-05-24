68f79969c60613f32a2510ace0339d5f237607c0
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-05-24 15:24:05 +0200

    Added code to log each name with its matching province.

--- index.js
+++ index.js
@@ -21,3 +21,10 @@ console.log("Names:");
 names.forEach(name => console.log(name));
 console.log("\nProvinces:");
 provinces.forEach(province => console.log(province));
+
+// Logging each name with matching province
+console.log("\nName with matching province:");
+names.forEach(name => {
+  const provinceIndex = names.indexOf(name);
+  console.log(`${name} (${provinces[provinceIndex]})`);
+});
